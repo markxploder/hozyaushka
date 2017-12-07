7d83ea94f42b5aa3901e916ca063fa5fe07e3bb4
markxploder <markxploder@gmail.com>
2017-12-07 18:41:42 +0200

    Footer

--- hoz/forms.py
+++ hoz/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import ReviewPost
+from .models import ReviewPost, RequestPost
 from django.forms import widgets
 from hoz.choices import *
 
@@ -11,6 +11,18 @@ class ReviewPostForm(forms.ModelForm):
         fields = ['author', 'text', 'rating']
         widgets = {
             'author': forms.TextInput(attrs={'placeholder': 'Имя'}),
+            'text': forms.Textarea(
+                attrs={'placeholder': 'Комментарий'}),
+        }
+
+class RequestPostForm(forms.ModelForm):
+
+    class Meta:
+        model = RequestPost
+        fields = ['name', 'phone', 'text']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Имя'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Телефон'}),
             'text': forms.Textarea(
                 attrs={'placeholder': 'Комментарий'}),
         }
