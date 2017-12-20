bff391c517136c540c5a8c00ed74ce43de238aa5
markxploder <markxploder@gmail.com>
2017-12-20 16:05:18 +0200

    Optiization

--- hoz/views.py
+++ hoz/views.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.cache import cache_page
 from django.utils import timezone
 from .models import ReviewPost, RequestPost, CallbackPost
 from .forms import ReviewPostForm, RequestPostForm, CallbackPostForm
 
+# @cache_page(900, cache='default', key_prefix='')
 def main(request):
     posts_reviews = ReviewPost.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     if request.method == "POST":
@@ -30,18 +32,6 @@ def main(request):
         form_request = RequestPostForm()
         form_callback = CallbackPostForm()
     return render(request, 'hoz/main.html', {'posts_reviews': posts_reviews[:3], 'form_reviews': form_reviews, 'form_request': form_request, 'form_callback': form_callback})
-
-# def callback(request, template):
-#     if request.method == "POST":
-#         form_callback = CallbackPostForm(request.POST)
-#         if form_callback.is_valid():
-#             callback_post = form_callback.save(commit=False)
-#             callback_post.published_date = timezone.now()
-#             callback_post.save()
-#             return redirect('main')
-#     else:
-#         form_callback = CallbackPostForm()
-#     return render(request, template, {'form_callback': form_callback})
 
 def furniture(request):
     if request.method == "POST":
